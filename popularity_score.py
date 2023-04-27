0da96d4d640d614bef8a58422475082c6b139858
Utkarsh_lal <utkarshredd@gmail.com>
2023-04-27 17:47:36 +0530

    Changed aggregation to product_id. Cassandra test pending

--- popularity_score.py
+++ popularity_score.py
@@ -36,9 +36,11 @@ def preprocessing(clicks_path, wishlist_path, cart_path, orders_completed_path):
                        'price', 'product_id', 'price']]
     
     # Brand wise aggregation
-    df_clicks_brand = df_clicks.copy()
-    df_clicks_brand['clicks'] = np.zeros(len(df_clicks_brand))
-    df_clicks_brand = df_clicks_brand.groupby('brand', as_index=False).agg({'clicks': 'count'})
+    df_clicks_product = df_clicks.copy()
+    df_clicks_product['clicks'] = np.zeros(len(df_clicks_product))
+    df_clicks_product = df_clicks_product.groupby('product_id', as_index=False).agg({'clicks': 'count'})
+    df_clicks_product['product_id'] = df_clicks_product['product_id'].astype(int)
+
 
     # Mapping brands to categories
     brand_to_category_dict = dict(zip(df_clicks['brand'], df_clicks['category']))
@@ -67,18 +69,19 @@ def preprocessing(clicks_path, wishlist_path, cart_path, orders_completed_path):
     df_order_completed.drop(['item_brand', 'item_id', 'item_name', 'coupon', 'currency', 'category_id', 'image_url', 'size', 'item_price', 'item_quantity'], axis=1, inplace=True)
     df_order_completed['price'] = df_order_completed['price'].astype(float)
     df_order_completed['quantity'] = df_order_completed['quantity'].astype(float)
+    df_order_completed['product_id'] = df_order_completed['product_id'].astype(int)
 
     df_order_completed['quantity'] = df_order_completed['quantity'].apply(lambda x: 1 if x == 0 else x)
     df_order_completed['category'] = df_order_completed['category'].apply(lambda x: 'not set' if x == '0' else x)
 
     # Brand wise aggregation
-    df_order_completed_brand = df_order_completed.copy()
-    df_order_completed_brand['no_of_unique_purchases'] = np.zeros(len(df_order_completed_brand))
+    df_order_completed_product = df_order_completed.copy()
+    df_order_completed_product['no_of_unique_purchases'] = np.zeros(len(df_order_completed_product))
 
-    df_grouped = df_order_completed_brand.groupby('brand', as_index=False).agg({'price':'sum', 'quantity':'sum', 'no_of_unique_purchases': 'count'})
+    df_grouped = df_order_completed_product.groupby('product_id', as_index=False).agg({'price':'sum', 'quantity':'sum', 'no_of_unique_purchases': 'count'})
     df_grouped.rename(columns={'quantity':'no_of_purchases'}, inplace=True)
     df_grouped.sort_values(by='no_of_unique_purchases', ascending=False, inplace=True)
-    df_order_completed_brand = df_grouped.copy()
+    df_order_completed_product = df_grouped.copy()
 
     ### Wishlist ###
     df_wishlist_cart = pd.read_csv(wishlist_path)
@@ -86,41 +89,43 @@ def preprocessing(clicks_path, wishlist_path, cart_path, orders_completed_path):
 
     # Brand wise aggregation
     df_wishlist = df_wishlist_cart
-    df_wishlist_brand = df_wishlist.copy()
-    df_wishlist_brand['no_of_adds_to_wishlist'] = np.zeros(len(df_wishlist_brand))
-    df_grouped = df_wishlist_brand.groupby('brand', as_index=False)[['brand', 'no_of_adds_to_wishlist', 'price']].\
-    agg({'price': 'sum', 'no_of_adds_to_wishlist': 'count'}).sort_values('no_of_adds_to_wishlist', ascending= False)
-    df_wishlist_brand = df_grouped.copy()
+    df_wishlist_product = df_wishlist.copy()
+    df_wishlist_product['no_of_adds_to_wishlist'] = np.zeros(len(df_wishlist_product))
+    df_grouped = df_wishlist_product.groupby('product_id', as_index=False)[['brand', 'no_of_adds_to_wishlist', 'price']].\
+        agg({'price': 'sum', 'no_of_adds_to_wishlist': 'count'}).sort_values('no_of_adds_to_wishlist', ascending= False)
+    df_grouped['product_id'] = df_grouped['product_id'].astype(int)
+    df_wishlist_product = df_grouped.copy()
 
     ### Cart ###
     df_cart = pd.read_csv(cart_path)
-    df_cart_brand = df_cart.copy()
-    df_cart_brand['no_of_adds_to_cart'] = np.zeros(len(df_cart_brand))
-    df_cart_brand = df_cart_brand.groupby('brand', as_index=False)[['category', 'price', 'quantity', 'no_of_adds_to_cart']]\
+    df_cart_product = df_cart.copy()
+    df_cart_product['no_of_adds_to_cart'] = np.zeros(len(df_cart_product))
+    df_cart_product = df_cart_product.groupby('product_id', as_index=False)[['category', 'price', 'quantity', 'no_of_adds_to_cart']]\
         .agg({'price': 'sum', 'quantity': 'sum', 'no_of_adds_to_cart': 'count'}).sort_values('no_of_adds_to_cart', ascending=False)
+    df_cart_product['product_id'] = df_cart_product['product_id'].astype(int)
     
 
     ### Joining All tables ###
     # Brand Tables
-    df_joined_brand = df_clicks_brand.merge(df_order_completed_brand, on='brand', how='inner', suffixes=('', '_order_completes')).\
-                                merge(df_wishlist_brand, on='brand', how='inner', suffixes=('', '_wishlist'))
-    df_joined_brand = df_joined_brand.merge(df_cart_brand, on='brand', how='inner', suffixes=('', '_cart'))
-    df_joined_brand.drop(['price', 'price_wishlist', 'price_cart', 'quantity'], axis=1, inplace=True)
-    df_ml = df_joined_brand[['brand', 'clicks', 'no_of_adds_to_wishlist', 'no_of_adds_to_cart', 'no_of_unique_purchases']].copy()
+    df_joined_product = df_clicks_product.merge(df_order_completed_product, on='product_id', how='inner', suffixes=('', '_order_completes')).\
+                            merge(df_wishlist_product, on='product_id', how='inner', suffixes=('', '_wishlist'))
+    df_joined_product = df_joined_product.merge(df_cart_product, on='product_id', how='inner', suffixes=('', '_cart'))
+    df_joined_product.drop(['price', 'price_wishlist', 'price_cart', 'quantity'], axis=1, inplace=True)
+    df_ml = df_joined_product[['product_id', 'clicks', 'no_of_adds_to_wishlist', 'no_of_adds_to_cart', 'no_of_unique_purchases']].copy()
+    
     normalize_cols = ['clicks', 'no_of_adds_to_wishlist', 'no_of_adds_to_cart', 'no_of_unique_purchases']
 
     for column in normalize_cols:
         df_ml[column] = (df_ml[column] - df_ml[column].min()) / (df_ml[column].max() - df_ml[column].min())    
     df_ml = df_ml.dropna()
     df_ml.rename(columns={'no_of_adds_to_wishlist': 'adds_to_cart_from_wishlist'}, inplace=True)
-    df_joined_brand.rename(columns={'no_of_adds_to_wishlist': 'adds_to_cart_from_wishlist'}, inplace=True)
-    
-    return df_ml, df_joined_brand
+    df_joined_product.rename(columns={'no_of_adds_to_wishlist': 'adds_to_cart_from_wishlist'}, inplace=True)
+    return df_ml, df_joined_product
 
 def train_compare_models():
 
-    df_ml, df_joined_brand = preprocessing(cfg.clicks_path, cfg.wishlist_path, cfg.cart_path, cfg.order_completed_path)
-    df_ml = df_ml.dropna()
+    df_ml, df_joined_product = preprocessing(cfg.clicks_path, cfg.wishlist_path, cfg.cart_path, cfg.order_completed_path)
+    #df_ml = df_ml.dropna()
     X = df_ml[['clicks', 'adds_to_cart_from_wishlist', 'no_of_adds_to_cart']]
     Y = df_ml['no_of_unique_purchases']
     x_train, x_test, y_train, y_test = train_test_split(X, Y,
@@ -131,7 +136,7 @@ def train_compare_models():
     rf_reg = RandomForestRegressor()
     rf_reg.fit(x_train, y_train)
     train_score = rf_reg.score(x_train, y_train)
-
+    print(train_score)
     rf_regressor = RandomForestRegressor(random_state = 123)
 
     params = {
@@ -145,21 +150,21 @@ def train_compare_models():
 
     print('Fitting and tuning Random Forest')
     tuned_rf_regressor = GridSearchCV(estimator=rf_regressor, 
-                    param_grid=params,
-                    scoring='neg_mean_absolute_error', 
-                    verbose=1, cv=15)
+                   param_grid=params,
+                   scoring='r2', 
+                   verbose=1, cv=15)
 
 
     tuned_rf_regressor.fit(x_train, y_train)
-    #print("Best parameters for Random Forest:", tuned_rf_regressor.best_params_)
+    print("Best parameters:", tuned_rf_regressor.best_params_)
     #print("\nLowest MAE: ", (-tuned_xgb.best_score_)**(1/2.0))
-    print("\nMAE: ", tuned_rf_regressor.best_score_)
-    rf_mae = tuned_rf_regressor.best_score_
+    print("\nR2: ", tuned_rf_regressor.best_score_)
+    rf_r2 = tuned_rf_regressor.best_score_
 
     #  Predicting the Test set results
-    y_preds = tuned_rf_regressor.best_estimator_.predict(x_test)
-    rf_test_accuracy = mae(y_test, y_preds)
-    print(f"MAE for RF on the testing data: {rf_test_accuracy}")
+    y_pred = tuned_rf_regressor.best_estimator_.predict(x_test)
+    rf_test_R2 = r2_score(y_test, y_pred)
+    print("\nR2 score for RF on test data: ", rf_test_R2)
 
     # Plotting the feature importance for RF
     importances = tuned_rf_regressor.best_estimator_.feature_importances_
@@ -172,7 +177,7 @@ def train_compare_models():
     plt.xlabel('Relative Importance')
     plt.savefig(f'{cfg.PLOTS_DIR}/RF_Feature_Importance.png', bbox_inches='tight')
 
-    #plt.show()
+    plt.show()
 
 
     importances_rf = importances.copy()
@@ -180,13 +185,118 @@ def train_compare_models():
     rf_imp.set_index('features', inplace=True)
     print(rf_imp)
 
-    xgb_test_accuracy = 100
 
-    if (rf_test_accuracy < xgb_test_accuracy):
+    ### XGBoost ####
+    print("Fitting and tuning XGBoost")
+    xgb = XGBRegressor(seed = 123)
+
+    params = { 
+        'max_depth': [3,6,10],
+        'learning_rate': [0.01, 0.05, 0.1],
+        'n_estimators': [100, 500, 1000],
+        'colsample_bytree': [0.3, 0.7]
+    }
+
+
+    tuned_xgb = GridSearchCV(estimator=xgb, 
+                    param_grid=params,
+                    scoring='r2', 
+                    verbose=1, cv=10)
+
+
+    tuned_xgb.fit(x_train, y_train)
+    print("Best parameters:", tuned_xgb.best_params_)
+    #print("\nLowest MAE: ", (-tuned_xgb.best_score_)**(1/2.0))
+    print("\R2 for XGB best estimator: ", tuned_xgb.best_score_)
+    xgb_mae = tuned_xgb.best_score_
+
+    # Predicting the Test set results
+    y_preds = tuned_xgb.best_estimator_.predict(x_test)
+    xgb_test_accuracy = mae(y_test, y_preds)
+    xgb_test_R2 = r2_score(y_test, y_preds)
+    print(f"MAE on the testing data: {xgb_test_accuracy}\n R2 score on testing data: {xgb_test_R2}")
+
+    # Plotting xgb feature importance
+    importances = tuned_xgb.best_estimator_.feature_importances_
+    indices = np.argsort(importances)
+    features = tuned_xgb.best_estimator_.feature_names_in_
+
+    plt.figsize = (6,6)
+    plt.title('Feature Importances')
+    plt.barh(range(len(indices)), importances[indices], color='g', align='center')
+    plt.yticks(range(len(indices)), [features[i] for i in indices])
+    plt.xlabel('Relative Importance')
+    plt.savefig(f"{cfg.PLOTS_DIR}/XGB_feature_importance.jpg", bbox_inches='tight')
+
+    plt.show()
+
+    importances_xgb = importances.copy()
+    xgb_imp = pd.DataFrame({'features': tuned_xgb.best_estimator_.feature_names_in_, 'importance': tuned_xgb.best_estimator_.feature_importances_})
+    xgb_imp
+    xgb_imp.set_index('features', inplace=True)
+
+    print(xgb_imp)
+    # xgb_test_accuracy = 100
+
+    ######## Fitting Huber Regressor ########
+    hbr = HuberRegressor()
+    print("Fitting and tuning Huber Regressor")
+    params = { 
+        'alpha': [0.0001, 0.001, 0.01],
+        'epsilon': [1.35, 1.20, 1.4],
+        'max_iter': [100, 500],
+        'tol': [1e-05, 1e-04, 1e-03]
+    }
+
+
+    tuned_hbr = GridSearchCV(estimator=hbr, 
+                    param_grid=params,
+                    scoring='r2', 
+                    verbose=1, cv=10)
+
+
+    tuned_hbr.fit(x_train, y_train)
+    print("Best parameters:", tuned_hbr.best_params_)
+    #print("\nLowest MAE: ", (-tuned_xgb.best_score_)**(1/2.0))
+    print("R2: ", tuned_hbr.best_score_)
+
+    y_preds = tuned_hbr.best_estimator_.predict(x_test)
+    hbr_test_accuracy = mae(y_test, y_preds)
+    hbr_test_R2 = r2_score(y_test, y_preds)
+    print(f"MAE on the testing data: {hbr_test_accuracy}\n R2 score: {hbr_test_R2}")
+
+    importances = tuned_hbr.best_estimator_.coef_
+    indices = np.argsort(importances)
+    features = X.columns
+    plt.title('Feature Importances')
+    plt.barh(range(len(indices)), importances[indices], color='b', align='center')
+    plt.yticks(range(len(indices)), [features[i] for i in indices])
+    plt.xlabel('Relative Importance')
+    plt.savefig(f"{cfg.PLOTS_DIR}/Huber_feature_importance.jpg", bbox_inches='tight')
+    plt.show()
+
+    importance_df_huber = pd.DataFrame({'feature': X.columns, 'weights': tuned_hbr.best_estimator_.coef_})
+    importance_df_huber.sort_values('weights', ascending=False, inplace=True)
+    importance_df_huber['weights'] = importance_df_huber['weights']/importance_df_huber['weights'].sum()
+    importance_df_huber.set_index('feature', inplace=True)
+    print(importance_df_huber)
+    dict1 = {}
+    dict1['RandomForest'] = abs(rf_test_R2)
+    dict1['XGBoost'] = abs(xgb_test_R2)
+    dict1['HuberRegressor'] = hbr_test_R2
+    df_results = pd.DataFrame.from_dict(dict1, orient='index', columns=['R2'])
+
+    print("Model with best R2: ", df_results['R2'].idxmax())
+
+    result = df_results['R2'].idxmax()
+    if (result == 'RandomForest'):
         print("Random Forest is giving better performance on Regression")
-        df_X = df_joined_brand[['brand', 'clicks', 'adds_to_cart_from_wishlist', 'no_of_adds_to_cart', 'no_of_purchases']].copy()
+        df_X = df_joined_product[['product_id', 'clicks', 'adds_to_cart_from_wishlist', 'no_of_adds_to_cart', 'no_of_purchases']].copy()
+        normalize_cols = ['clicks', 'adds_to_cart_from_wishlist', 'no_of_adds_to_cart', 'no_of_purchases']
 
-        df_X = df_joined_brand.apply(lambda x: (x-x.min())/(x.max()-x.min()) if(type(x[0]) != str) else x)
+        for column in normalize_cols:
+            df_X[column] = (df_X[column] - df_X[column].min()) / (df_X[column].max() - df_X[column].min())    
+
         if (max(rf_imp['importance']) > 0.5):
             w_purchase = max(rf_imp['importance']) - 0.15
         else:
@@ -201,11 +311,15 @@ def train_compare_models():
 
         return df_popularity_fin
 
-    else:
+    elif(result == 'XGBoost'):
         print("XGBoost is giving better performance on Regression")
-        df_X = df_joined_brand[['brand', 'clicks', 'adds_to_cart_from_wishlist', 'no_of_adds_to_cart', 'no_of_purchases']].copy()
+        df_X = df_joined_product[['product_id', 'clicks', 'adds_to_cart_from_wishlist', 'no_of_adds_to_cart', 'no_of_purchases']].copy()
 
-        df_X = df_joined_brand.apply(lambda x: (x-x.min())/(x.max()-x.min()) if(type(x[0]) != str) else x)
+        normalize_cols = ['clicks', 'adds_to_cart_from_wishlist', 'no_of_adds_to_cart', 'no_of_purchases']
+
+        for column in normalize_cols:
+            df_X[column] = (df_X[column] - df_X[column].min()) / (df_X[column].max() - df_X[column].min())    
+
         if (max(xgb_imp['importance']) > 0.5):
             w_purchase = max(xgb_imp['importance']) - 0.15
         else:
@@ -220,7 +334,30 @@ def train_compare_models():
         df_popularity_fin.to_csv(f'{cfg.OUTPUT_DIR}/popularity_score.csv', index=False)
 
         return df_popularity_fin
+    
+    else:
+        print("HuberRegressor is giving better performance on Regression")
+        df_X = df_joined_product[['product_id', 'clicks', 'adds_to_cart_from_wishlist', 'no_of_adds_to_cart', 'no_of_purchases']].copy()
 
+        normalize_cols = ['clicks', 'adds_to_cart_from_wishlist', 'no_of_adds_to_cart', 'no_of_purchases']
+
+        for column in normalize_cols:
+            df_X[column] = (df_X[column] - df_X[column].min()) / (df_X[column].max() - df_X[column].min())    
+
+        if (max(importance_df_huber['weights']) > 0.5):
+            w_purchase = max(importance_df_huber['weights']) - 0.15
+        else:
+            w_purchase = max(importance_df_huber['weights']) + 0.15
+        w_clicks = importance_df_huber.loc['clicks', 'weights'] * (1 - w_purchase)
+        w_wishlist = importance_df_huber.loc['adds_to_cart_from_wishlist', 'weights'] * (1 - w_purchase)
+        w_cart = importance_df_huber.loc['no_of_adds_to_cart', 'weights'] * (1 - w_purchase)
+        df_popularity_fin = df_X
+
+        df_popularity_fin['popularity_score'] = (df_X['no_of_purchases'] * w_purchase + df_X['clicks'] * w_clicks + df_X['adds_to_cart_from_wishlist'] * w_wishlist + df_X['no_of_adds_to_cart'] * w_cart)/(w_purchase + w_clicks + w_wishlist + w_cart)
+        df_popularity_fin.sort_values('popularity_score', ascending=False, inplace=True)
+        df_popularity_fin.to_csv(f'{cfg.OUTPUT_DIR}/popularity_score.csv', index=False)
+
+        return df_popularity_fin
 
 
 
